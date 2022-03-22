3baf268ddc006a9d8477d4112013256c5583c529
atuan1605 <anhtuan097856@gmail.com>
2022-03-22 21:26:18 +0700

    update

--- javacore/lesson1/first-app/src/Main.java
+++ javacore/lesson1/first-app/src/Main.java
@@ -1,8 +1,69 @@
 public class Main {
-
+    public int age; // day la bien toan cuc
+    public static final double PI = 3.14;
     public static void main(String[] args) {
-	    System.out.println("Hello world");
-        System.out.println("Xin chào");
-        System.out.println("Tên tôi là Lê Anh Tuấn");
+
+
+
+//       int age; //khai báo biến, bien cuc bo
+//       age = 26;
+//
+//        System.out.println(age);
+//
+//        long salary = 1000000L;
+//        System.out.println("Lương của tôi là : "+salary);
+//
+//        float weight =45.5f;
+//        double height= 1.6;
+//
+//        char c = 'c';
+//        char five ='5';
+//        char s = 65;
+//        System.out.println(s);
+
+
+//        double height = 1.5;
+//        double weight =40;
+//
+//        double bmi = weight/(height*height);
+//
+//        System.out.println(bmi);
+//
+//
+//        double m = 9;
+//        double c = 9;
+//        double p = 8;
+//
+//        double all= (m+c+p)/3;
+//        System.out.println(all);
+//
+//        System.out.println("Pi: " + PI);// hang so
+//
+//        System.out.println("xin  chào tôi \n \\ tên là \"Tuấn\"");
+
+        int a = 5;
+        int b = 2;
+
+        System.out.println(a%2 == 0 ? a + " là số chẵn" : a + " là số lẻ ");
+
+        int age = 17;
+        System.out.println(age >= 18 ? " bạn đủ tuổi bầu cử" : " bạn chưa đủ tuổi");
+
+//        boolean check = a == b ;
+//
+//        a += b;// a + b= b
+//
+//        System.out.println(a != b);
+//        System.out.println(check);
+
+//        double a1 = (double) a;// ep kieu
+//
+//        double c =(double) a/b;
+//        System.out.println(c);
+//
+//        int c1 = (int)c;
+//        System.out.println(c1);
+
+
     }
 }
