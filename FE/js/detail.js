ffcb3be4eb84c4e4e886537e18a67b83b1c0d9c7
atuan1605 <anhtuan097856@gmail.com>
2022-08-13 09:31:18 +0700

    up

--- FE/js/detail.js
+++ FE/js/detail.js
@@ -1,4 +1,4 @@
-const URL_API = "https://course-api-springboot.herokuapp.com/api/course";
+const URL_API = "https://course-api-springboot.herokuapp.com/api/course"
 const URL_SUPPORTER_API = "https://course-api-springboot.herokuapp.com/api/supporter";
 
 const linkParams = new URLSearchParams(window.location.search)
